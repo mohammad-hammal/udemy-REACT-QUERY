683867ca6be6b7974baba8529413127339646f8d
mohammad-hammal <mohammad.hammal@yhh.ae>
2023-01-15 20:52:46 +0300

    Start

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,18 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { Posts } from './Posts';
+import './App.css';
+
+const client = new QueryClient();
 
 function App() {
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 async function fetchPosts() {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
   );
   return response.json();
 }
 
 export function Posts() {
+  const { data, load } = useQuery(['posts'], fetchPosts);
+
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  if (!data) return <h1>Loading ....</h1>;
 
   return (
     <>
